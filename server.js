06dba1e3e17233f7a8d4774dd043ad3c88da5ba4
Emmanuel Ashaolu <tohbyy@gmail.com>
2019-11-02 17:48:26 +0100

    Express server setup with initial routes

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
+
+//Connect Database
+connectDB();
 
 app.get('/', (req, res) => res.json({msg: 'Welcome to Contact Keeper API'}));
 
+//Define routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
